3f0b6b996fa1ad86673c7c43c7f6d78313fe6b92
Breno <bhfs.2908@gmail.com>
2021-10-11 11:26:44 -0300

    feat:add utils

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { api } from "../services/api";
+import convertDurationToTimeString from "../utils/convertDurationToTimeString";
 
 interface Episode {
   id: string;
   title: string;
   members: string;
+  published_at: string;
 }
 interface HomeProps {
   episodes: Episode[];
 }
 
 export default function Home(props: HomeProps) {
-  return <h1>Index</h1>;
+  return (
+    <>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -23,9 +32,23 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   });
 
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   };
