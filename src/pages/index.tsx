e276686adf2e7df203c2595799adb67c909aa3b9
Breno <bhfs.2908@gmail.com>
2021-10-11 11:02:37 -0300

    feat:add services

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
-import Header from "../components/Header";
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
-export default function Home() {
+interface Episode {
+  id: string;
+  title: string;
+  members: string;
+}
+interface HomeProps {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return <h1>Index</h1>;
 }
 
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
 
   return {
     props: {
@@ -14,4 +29,4 @@ export async function getStaticProps() {
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
